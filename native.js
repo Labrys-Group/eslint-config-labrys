c010f1da15e8b8eeac36e085c2eb4400b0d47fb1
Aidan Kinzett <aidan@labrys.io>
2023-08-29 18:01:53 +1000

    docs: add note to native config

--- native.js
+++ native.js
@@ -1,5 +1,8 @@
 /**
  * @type {import("eslint").Linter.Config}
+ * 
+ * This requires installing `@typescript-eslint/eslint-plugin` and `@typescript-eslint/parser` since we are
+ * extending [`universe/native`](https://github.com/expo/expo/tree/master/packages/eslint-config-universe).
  */
 module.exports = {
     extends: ["./base.js", "universe/native"],
