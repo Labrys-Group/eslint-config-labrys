ab4a66b04d99c3efd8cc3aaae7d9fed73b64cf1c
Aidan Kinzett <aidan@labrys.io>
2023-08-29 18:15:36 +1000

    chore: update rule overrides

--- base.js
+++ base.js
@@ -3,32 +3,21 @@
  */
 module.exports = {
   extends: [
-    "turbo",
-    "prettier",
+    "eslint:recommended",
     "plugin:eslint-comments/recommended",
     "plugin:import/recommended",
     "plugin:import/typescript",
+    "turbo",
+    "prettier",
   ],
   rules: {
-    "arrow-body-style": 2,
-    
     // turbo
     "turbo/no-undeclared-env-vars": "off", // off since we use dotenv files
 
     // eslint-comments
     "eslint-comments/require-description": "off",
     "eslint-comments/no-unused-disable": "error",
-
-    // import
-    "import/extensions": 0,
-    "import/no-unresolved": 0,
-    "import/order": [
-        2,
-        {
-          "newlines-between": "always",
-          alphabetize: { order: "asc", caseInsensitive: true },
-        },
-      ],
+    "eslint-comments/no-unused-enable": "error",
   },
   settings: {
     "import/resolver": {
